8c578b61a27feb15a1945ce64b38f5770d3beb48
Gianluca Carbone <glc.carbone@gmail.com>
2026-06-04 15:24:20 +0200

    fix: aggiungi challenge/approach/result al fillable del Model

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -8,16 +8,19 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 class Project extends Model
 {
     protected $fillable = [
-        'title',
-        'slug',
-        'client_name',
-        'category',
-        'excerpt',
-        'content',
-        'project_url',
-        'cover_image',
-        'is_featured',
-    ];
+    'title',
+    'slug',
+    'client_name',
+    'category',
+    'excerpt',
+    'content',
+    'challenge',
+    'approach',
+    'result',
+    'project_url',
+    'cover_image',
+    'is_featured',
+];
 
     protected $casts = [
         'is_featured' => 'boolean',
